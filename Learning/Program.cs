542135caa8ac762218055d49abe3f6f706aa4a5c
Егор Романченко <egor.gmag@yandex.ru>
2021-05-09 14:04:21 +0700

    zad2

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -6,7 +6,26 @@ namespace Learning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool p = true;
+            Console.WriteLine("Введите число");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    p = false;
+                    break;
+                }
+            }
+            if (p)
+            {
+                Console.WriteLine("Число простое");
+            }
+            else
+            {
+                Console.WriteLine("Число не простое");
+            }
+            Console.ReadKey();
         }
     }
 }
